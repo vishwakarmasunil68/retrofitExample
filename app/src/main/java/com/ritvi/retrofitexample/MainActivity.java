dfc4a2aa5a2df44ca826bf304bb3873f5984840a
sunil <Sunil112!((#>
2018-03-09 12:28:45 +0530

    09-03-2018

--- app/src/main/java/com/ritvi/retrofitexample/MainActivity.java
+++ app/src/main/java/com/ritvi/retrofitexample/MainActivity.java
@@ -1,10 +1,14 @@
 package com.ritvi.retrofitexample;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
     Button button1,button2,button3;
@@ -12,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(), "5e2ac978-e905-426c-b268-6b85e161e025",
+                Analytics.class, Crashes.class);
 
         button1=findViewById(R.id.button1);
         button2=findViewById(R.id.button2);
